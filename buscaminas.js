335d8cb079589171c9ddb42280b45540a5e957dd
[emirand20] <[emirand20@jaumebalmes.net]>
2022-10-24 20:03:39 +0200

    inProgress

---
+++ buscaminas.js
@@ -0,0 +1,14 @@
+function inicialitzaJoc() {
+    let inputX = document.getElementById("inputX").valueAsNumber;
+    let inputY = document.getElementById("inputY").valueAsNumber;
+    //recorrer hasta que sean los numeros puestos
+    let table = document.createElement('table');
+    
+    /*for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+            if (!(i == 0 && j == 0)) {
+                matrix[inputX + i][inputY + j].style.backgroundColor = "red";
+            }
+        }
+    }*/
+}
