e2ffa1a87f787fac101191ac24e7f7ebfb297a8f
[emirand20] <[emirand20@jaumebalmes.net]>
2022-11-21 20:11:04 +0100

    part3 acabada

--- buscaminas.js
+++ buscaminas.js
@@ -1,4 +1,6 @@
-function inicialitzaJoc() {
+const colorMina = "blue"; //inicializamos el color de la celda
+
+function inicialitzaJoc(mines) {
     //esto hace que cada vez que creamos un nuevo tamblero en el caso de ya haya uno no lo sobrescribamos
     if (document.getElementsByTagName("table").length != 0) {
         document.getElementsByTagName("table")[0].remove();
@@ -17,7 +19,9 @@ function inicialitzaJoc() {
         for (let y = 0; y < inputY; y++) {
             //creacion td
             let td = document.createElement("td");
-            tr.appendChild(td); 
+            td.onclick = clickCoordenada.bind(this,mines,x,y); //
+            tr.appendChild(td);
+            
         }
         tbody.appendChild(tr);
     }
@@ -25,10 +29,25 @@ function inicialitzaJoc() {
     tablero.setAttribute("width", "20%");
     tablero.setAttribute("height", "20%");
     tablero.setAttribute("border", 1);
-    
+
     tablero.appendChild(tbody);
     contenedorBody.appendChild(tablero);
 };
+
+function clickCoordenada(mines,x,y) {
+    imprimeixCoordenada(x,y)
+    imprimeixMina(mines,x,y)
+    
+}
+function imprimeixCoordenada(x,y) {
+    console.log(x,y);
+}
+
+function imprimeixMina(mines,x,y) {
+    // mines[x][y]
+    //
+}
+
 function pintaTablero() {
     let rows = document.getElementsByTagName("tbody")[0].children;
     let matrix = [];
@@ -65,14 +84,23 @@ function inicialitzaMines(nMines, midaX, midaY) {
 
     return arrayBobas;
 }
-function coordenadas(){
-
+function coordenada() {
+    let celda = document.getElementsByTagName("table")[0];
+    celda.addEventListener("click", function (event) {
+        if (event.target.style.backgroundColor == colorMina) {
+            console.log(event.target.id + "es mina");
+        } else {
+            console.log(event.target.id + "no es mina");
+        }
+    });
 }
 function inicialitza() {
-    inicialitzaJoc();
+    
     let x = document.getElementById("inputX").valueAsNumber;
     let y = document.getElementById("inputY").valueAsNumber;
     let creacionMinas = document.getElementById("minas").valueAsNumber;
     mines = inicialitzaMines(creacionMinas, x, y);
+    inicialitzaJoc(mines);
     pintaTablero(mines);
+    coordenada();
 }
